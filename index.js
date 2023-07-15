1dd8ff13e0370472b6d8351ecb3ffb672f2e7822
Theodore Smith <tjs7321@gmail.com>
2023-07-15 06:01:00 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+// get dodger
+
+const dodger = document.getElementById('dodger')
+
+dodger.style.backgroundColor = "#FF69B4"
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+
+// function for moveDodgerLeft
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
